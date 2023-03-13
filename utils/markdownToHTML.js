8bfd1c3a712728f7579ae8d919f7e37d55f7c3a0
Anuradha Kumari <2anuradha9@gmail.com>
2023-03-13 15:08:12 +0100

    markdown to json

--- utils/markdownToHTML.js
+++ utils/markdownToHTML.js
@@ -4,4 +4,4 @@ import html from 'remark-html';
 export default async function markdownToHtml(markdown) {
   const result = await remark().use(html).process(markdown);
   return result.toString();
-}
+} 
